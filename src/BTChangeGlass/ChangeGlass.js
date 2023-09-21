e9f697dd14c78cc9e5c4490ab395b9ad84af8517
Trung <trungnguyen.02082000@gmail.com>
2023-09-21 09:13:46 +0700

    hiển thị kính lên UI

--- src/BTChangeGlass/ChangeGlass.js
+++ src/BTChangeGlass/ChangeGlass.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import css from "./style.module.css"
+import { megeClassName } from "../utils/index"
 
 export default class ChangeGlass extends Component {
     mangSP = [
@@ -66,13 +68,57 @@ export default class ChangeGlass extends Component {
             "desc": "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. "
         }
     ]
+    state = {
+        styleGlass: {
+            position: "absolute",
+            top: "100px",
+            left: "240px",
+            width: "165px",
+            opacity: "0.5"
+        }
+
+    }
+    handleGlass = (possition) => {
+        this.setState({
+            glass: possition
+        })
+    }
+    hienThiSP = () => {
+        return this.mangSP.map((glass) => {
+            let { url } = glass;
+            return <div className="col-2">
+                <img className={megeClassName(css.glassFont)} src={url} alt="" />
+            </div>
+        })
+    }
     render() {
+
         return (
             <div>
-                <div className="col-2">
+                <header><h1>TRY GLASSES APP ONLINE</h1></header>
+                <div className="row">
+                    <div className="col-6">
+                        <img className={megeClassName(css.imageFont, css.possition)} src="./glassesImage/model.jpg" alt="" />
+                        <img style={this.state.styleGlass} src="./glassesImage/v1.png" alt="" />
+
+                    </div>
+                    <div className="col-6">
+                        <img className={css["imageFont"]} src="./glassesImage/model.jpg" alt="" />
+                    </div>
+                </div>
+                <div className={megeClassName("row", css.area)}>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v1.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v2.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v3.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v4.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v5.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v6.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v7.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v8.png" alt="" /></button>
+                    <button className={css["btnGlass"]}><img className={css["glassFont"]} src="./glassesImage/v9.png" alt="" /></button>
+
 
                 </div>
-
             </div>
         )
     }
